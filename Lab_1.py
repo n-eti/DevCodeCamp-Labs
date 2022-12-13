04cfb37a1ce012c2880d498db73325b98bea58cc
Nada Atieh <nada.s.atieh@gmail.com>
2022-12-13 13:50:12 -0600

    Added Lab 1 File

---
+++ Lab_1.py
@@ -0,0 +1,9 @@
+favorite_number = 5
+
+#Declare a variable called favorite_number and store your favorite or lucky number within it. 
+#Use the random module to generate a random number between a range that includes your favorite number.
+#Determine how many numbers away the random number was from your favorite number
+#Send a screenshot of your completed code to your instructor chat channel.
+
+import random
+favorite_number=
