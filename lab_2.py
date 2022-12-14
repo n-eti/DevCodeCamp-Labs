e51088133ca2e9d5c8232af9384a05d391845628
Nada Atieh <nada.s.atieh@gmail.com>
2022-12-14 14:07:14 -0600

    Took a string as input and returned the string reversed

---
+++ lab_2.py
@@ -0,0 +1,24 @@
+# Task 1: Reverse a String
+# (HINT: Review the Algorithms + Problem Solving Lecture!)
+# Write code that takes a string as input and returns the string reversed
+# i.e. “Hello” will be returned as “olleH”
+
+word="Christmas"
+
+# word[0] is "C"
+# word[1] is "H" 
+# word[2] is "R" 
+# word[3] is "I" 
+# word[4] is "S" 
+# word[5] is "T" 
+# word[6] is "M" 
+# word[7] is "A" 
+# word[8] is "S" 
+
+# reversed_word = word[8]+word[7]+word[6]+word[5]+word[4]+word[3]+word[2]+word[1]+word[0]
+# print(reversed_word)
+
+word="Christmas"
+reversed_word= word[len(word) -1]
+print=reversed_word
+
